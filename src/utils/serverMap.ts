8e88f57bb9119b3b5d3fe68188932374890d7162
jiiiong <jiiiong@qq.com>
2024-08-11 00:35:57 +0800

    使用 cors proxy 绕过 https 无法请求 http

--- src/utils/serverMap.ts
+++ src/utils/serverMap.ts
@@ -1,4 +1,4 @@
 const corsProxy = 'https://api.allorigins.win/raw?url=http://106.14.223.52/api';
-const publicProxy = 'http://106.14.223.52/public';
+const publicProxy = 'https://api.allorigins.win/raw?url=http://106.14.223.52/public';
 export const apiOrigin = (process.env.NODE_ENV === 'development') ? '/api' : corsProxy;
 export const publicOrigin = (process.env.NODE_ENV === 'development') ? '/public' : publicProxy;
