00e247da06fd3a18e19f9034619351666be36f70
jiiiong <jiiiong@qq.com>
2024-08-11 00:02:56 +0800

    在 https 中请求 http 资源是不被允许的； 再套一层代理

--- src/utils/serverMap.ts
+++ src/utils/serverMap.ts
@@ -1,3 +1,3 @@
-const corsProxy = 'http://106.14.223.52/api';
+const corsProxy = 'https://api.allorigins.win/raw?url=http://106.14.223.52/api';
 export const apiOrigin = (process.env.NODE_ENV === 'development') ? '/api' : corsProxy;
 export const publicOrigin = (process.env.NODE_ENV === 'development') ? '/api' : corsProxy;
