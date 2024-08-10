d84300cb300d8b5b4b7f318953c2ee88a994b311
jiiiong <jiiiong@qq.com>
2024-08-10 22:08:25 +0800

    配置了一个 CORS Proxy 尝试绕过源服务器的 CORS配置

--- src/utils/serverMap.ts
+++ src/utils/serverMap.ts
@@ -1,2 +1,3 @@
-export const apiOrigin = (process.env.NODE_ENV === 'development') ? '/api' : 'http://106.14.223.52';
-export const publicOrigin = (process.env.NODE_ENV === 'development') ? '/api' : 'http://106.14.223.52';
+const corsProxy = 'https://crossorigin.me/';
+export const apiOrigin = (process.env.NODE_ENV === 'development') ? '/api' : corsProxy + 'http://106.14.223.52';
+export const publicOrigin = (process.env.NODE_ENV === 'development') ? '/api' : corsProxy + 'http://106.14.223.52';
